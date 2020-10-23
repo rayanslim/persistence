10bc461e9e24499b85f6985328fe83ab8e22d0be
Rayan Slim <rayan@Rayans-iMac.local>
2020-10-23 13:47:09 -0400

    first commit

---
+++ src/main/java/com/example/persis/PersisApplication.java
@@ -0,0 +1,14 @@
+package com.example.persis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersisApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/example/persis/PersisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.persis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
